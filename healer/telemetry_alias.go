18c99d19f4750240cf9c16371a642e14778e795f
Jon-Arve Constantine Grønsberg-Ovesen <jonovesen@gmail.com>
2026-08-26 22:44:34 +0200

    fix(healer): import net/http in PrometheusHandler alias

--- healer/telemetry_alias.go
+++ healer/telemetry_alias.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 // PrometheusHandler aliases PromHandler so startHTTPServer in main.go compiles.
 func (t *Telemetry) PrometheusHandler() http.Handler {
 	return t.PromHandler()
